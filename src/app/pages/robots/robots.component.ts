454ca60ba7399cc48423f8083058bc948bdd5f31
Maxim Efremov <maxefremov04092004@gmail.com>
2025-05-14 18:38:03 +0300

    chore: Добавление всех основных сервисов, компонентов, сторов

---
+++ src/app/pages/robots/robots.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-robots',
+  imports: [],
+  templateUrl: './robots.component.html',
+  styleUrl: './robots.component.scss'
+})
+export class RobotsComponent {
+
+}
