808bd6c4e5b4eca6843f47a41ccb589ebaf79772
Maxim Efremov <maxefremov04092004@gmail.com>
2025-05-18 00:22:27 +0300

    feat(pages/gallery): Добавлена страница со слайдером

--- src/app/pages/galery/galery.component.ts
+++ src/app/pages/galery/galery.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-galery',
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './galery.component.html',
-  styleUrl: './galery.component.scss'
+  styleUrl: './galery.component.scss',
 })
 export class GaleryComponent {
+  public imgs = [
+    'assets/1.png',
+    'assets/2.png',
+    'assets/3.png',
+    'assets/4.png',
+    'assets/5.png',
+  ];
 
+  public currentIndex = 0;
+
+  public prevSlide() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.imgs.length) % this.imgs.length;
+  }
+  public nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.imgs.length;
+  }
 }
